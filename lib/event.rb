c8c95b43aac54d83903df5c6adfdc6d69be436d0
Regina Casias <reginaannvigil@gmail.com>
2021-12-20 17:33:19 -0700

    Added most_supplies

--- lib/event.rb
+++ lib/event.rb
@@ -10,4 +10,22 @@ class Event
     @attendees = attendees
   end
 
+  def attendee_names
+    @attendees.map do |attendee|
+      attendee.name
+    end
+  end
+
+  def craft_with_most_supplies
+    craft_list = {}
+    @crafts.map do |craft|
+        craft_list[craft.name] = craft.required_supplies.count
+    end
+    most_craft = craft_list.max_by do |key,v|
+      v
+    end
+    most_craft[0]
+    # require'pry';binding.pry
+  end
+
 end
